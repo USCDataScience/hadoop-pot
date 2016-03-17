3ef2922eac2ef0b620f3a5881d5e163440d6ddc0
Michael Joyce <mltjoyce@gmail.com>
2016-03-17 09:22:06 -0700

    Similarity job clean up

--- src/main/java/gov/nasa/jpl/memex/pooledtimeseries/SimilarityCalculation.java
+++ src/main/java/gov/nasa/jpl/memex/pooledtimeseries/SimilarityCalculation.java
@@ -3,14 +3,12 @@ package gov.nasa.jpl.memex.pooledtimeseries;
 import java.io.IOException;
 import java.util.*;
 import java.io.*;
-//import java.nio.file.Path;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
-//import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.util.*;
 import org.apache.hadoop.util.*;
@@ -34,12 +32,8 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.video.Video;
 
 public class SimilarityCalculation {
-    //public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
     public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-        //
-        //public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException, NumberFormatException {
-            //output.collect(value, new Text(ofVector));
             System.out.println(value.toString());
             Configuration conf = context.getConfiguration();
             String meanDistsPath = conf.get("meanDistsFilePath");
@@ -77,26 +71,11 @@ public class SimilarityCalculation {
                 counter++;
             }
 
-            //double[][] similarities = PoT.calculateSimilarities(fvList);
-            //System.out.println("------------");
-            //System.out.println(similarities[0][0] + " " + similarities[0][1]);
-            //System.out.println(similarities[1][0] + " " + similarities[1][1]);
-            //System.out.println("------------");
-
             double similarity = PoT.kernelDistance(fvList.get(0), fvList.get(1), meanDists);
-
-            // The below is for dumping the file paths as SHA256 hex strings. For now this is being defaulted
-            // to the full file paths because the other results are rather difficult for a human to read. =)
-            //String newValue = DigestUtils.sha256Hex(videoPaths[0]).toUpperCase() + ',' + DigestUtils.sha256Hex(videoPaths[1]).toUpperCase();
-            //output.collect(new Text(newValue), new Text(String.valueOf(similarities[0][1])));
-            //output.collect(new Text(newValue), new Text(String.valueOf(similarities[0][1])));
             
             File p1 = new File(videoPaths[0]);
             File p2 = new File(videoPaths[1]);
-            //output.collect(new Text(p1.getName() + ',' + p2.getName()), new Text(String.valueOf(similarities[0][1])));
             context.write(new Text(p1.getName() + ',' + p2.getName()), new Text(String.valueOf(similarity)));
-
-            //output.collect(value, new Text(String.valueOf(similarities[0][1])));
         }
     }
 
@@ -107,30 +86,10 @@ public class SimilarityCalculation {
         Configuration baseConf = new Configuration();
         baseConf.set("mapred.reduce.tasks", "0");
         baseConf.set("meanDistsFilePath", args[2]);
-        //JobConf conf = new JobConf(baseConf, SimilarityCalculation.class);
 
-        //conf.setJobName("similarity_calc");
-
-        //conf.setOutputKeyClass(Text.class);
-        //conf.setOutputValueClass(Text.class);
-
-        //conf.setMapperClass(Map.class);
-
-        //conf.setInputFormat(TextInputFormat.class);
-        //conf.setOutputFormat(TextOutputFormat.class);
-
-        //FileInputFormat.setInputPaths(conf, new Path(args[0]));
-        //FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-
-        //conf.set("meanDistsFilePath", args[2]);
-
-        //JobClient.runJob(conf);
-
-        // Create a new Job
         Job job = Job.getInstance(baseConf);
         job.setJarByClass(SimilarityCalculation.class);
 
-        // Specify various job-specific parameters     
         job.setJobName("similarity_calc");
 
         job.setOutputKeyClass(Text.class);
@@ -139,33 +98,12 @@ public class SimilarityCalculation {
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        //job.setInputPath(new Path(args[0]));
-        //job.setOutputPath(new Path(args[1]));
         FileInputFormat.setInputPaths(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.setMapperClass(Map.class);
 
-        // Submit the job, then poll for progress until the job is complete
         job.waitForCompletion(true);
-
-         //Job job = Job.getInstance(new Configuration());
-
-    //job.setOutputKeyClass(Text.class);
-    //job.setOutputValueClass(IntWritable.class);
-
-    //job.setMapperClass(Map.class);
-    //job.setReducerClass(Reduce.class);
-
-    //job.setInputFormatClass(TextInputFormat.class);
-    //job.setOutputFormatClass(TextOutputFormat.class);
-
-    //FileInputFormat.setInputPaths(job, new Path(args[0]));
-    //FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-    //job.setJarByClass(WordCount.class);
-
-    //job.submit();
     }
 }
 
