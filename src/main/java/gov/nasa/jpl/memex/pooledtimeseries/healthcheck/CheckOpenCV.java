329d5c752ce197cde4c2bf96c3f75ee203874f32
smadha <msharan@usc.edu>
2016-06-05 18:09:37 -0700

    adding video capture in healtcheck

--- src/main/java/gov/nasa/jpl/memex/pooledtimeseries/healthcheck/CheckOpenCV.java
+++ src/main/java/gov/nasa/jpl/memex/pooledtimeseries/healthcheck/CheckOpenCV.java
@@ -3,6 +3,7 @@ package gov.nasa.jpl.memex.pooledtimeseries.healthcheck;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
 
 public class CheckOpenCV {
 	
@@ -10,5 +11,15 @@ public class CheckOpenCV {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
 		System.out.println("mat = " + mat.dump());
+
+		String filename = args[0];
+
+		System.out.println("opening video file " + filename);
+		VideoCapture capture = new VideoCapture(filename.toString());
+
+		if (!capture.isOpened()) {
+			System.out.println("video file " + filename + " could not be opened.");
+
+		}
 	}
 }
