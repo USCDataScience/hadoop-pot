6c32aa6fd75430043e34f7cea520b84f853934fd
smadha <msharan@usc.edu>
2016-08-25 03:46:27 -0700

    setting picture size enough for num_videos

--- src/main/bin/similarity_heatmap.py
+++ src/main/bin/similarity_heatmap.py
@@ -3,7 +3,7 @@ import pylab as pl
 import sys
 
 path_to_sim_mat = sys.argv[1]
-num_videos = sys.argv[2]
+num_videos = int(sys.argv[2])
 print path_to_sim_mat
 print num_videos
 
@@ -12,8 +12,12 @@ print num_videos
 # skip first column so usecols=range(1 , num_videos),
 # paint only upper half filling_values=0)
 data = np.genfromtxt(path_to_sim_mat,
-                  delimiter=",", skip_header=1, usecols=range(1 , int(num_videos)+1),
+                  delimiter=",", skip_header=1, usecols=range(1 , num_videos+1),
                   filling_values=0)
+
+#setting picture size enough for num_videos
+if num_videos >= 100:
+    pl.figure(figsize = (num_videos/10,num_videos/10), dpi = 5)
 
 #use single color blue
 pl.pcolor(data, cmap=pl.cm.Blues, alpha=0.8)
